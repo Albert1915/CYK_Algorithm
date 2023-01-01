f9a5c1417ca93ddc0d1b4c5b4c989f9209ec83d8
Albert Okario <76970766+Albert1915@users.noreply.github.com>
2023-01-01 19:53:09 +0700

    Update web.py

--- view/web.py
+++ view/web.py
@@ -10,14 +10,14 @@ from controller.cyk_algorithm.cyk_parse import parse
 def run_streamlit():
     # title for the web
     title = 'Syntactic Parsing of Indonesian Sentences Using CYK Algorithm'
+    title2 = 'KELOMPOK 2 KELAS E'
 
     # setup the web configuration
     st.set_page_config(layout='wide', page_title=title, menu_items={
         'About': f"""
         ### {title}
-        Made with :heart: by Group 3 in Class A  
-        Language and Automata Theory Subject  
-        GitHub: https://github.com/tudemaha/cyk_algorithm
+        Made by Group 2 in Class E  
+        GitHub: https://github.com/Albert1915/CYK_Algorithm
         """
     })
     
@@ -28,17 +28,14 @@ def run_streamlit():
 
     # web title
     st.write(f"<h1 style='text-align:center; '>{title}</h1>", unsafe_allow_html=True)
+    st.write(f"<h2 style='text-align:center; '>{title2}</h2>", unsafe_allow_html=True)
+    # Link
+    st.write("<h5 style='text-align: center;'><a href='https://github.com/Albert.1915/CFG_Parsing'>Albert1915/CFG_Parsing</a></h2>", unsafe_allow_html=True)
 
-    # split web into two columns, left column displays the cnf rule, right column displays the filling table
-    col1, col2 = st.columns(2, gap='small')
+    
+    st.write("### CNF Rules:")
+    st.write(raw_cfg)
 
-    # prepre the left column
-    with col1:
-        st.write("### CNF Rules:")
-        st.write(raw_cfg)
-
-    # prepare the right column
-    with col2:
         # the input sentence text field
         string_input = st.text_input('Input Sentence')
         # convert sentence into list
